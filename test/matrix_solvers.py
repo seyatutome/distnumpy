d00b21b2bd8ed188266dad42ba2a48f8d02ac111
madsbk <madsbk@797bad1c-92e7-11de-9a30-078c9dd406eb>
2009-10-06 09:54:27 +0000

    matrix_slovers.py still dont use diag, but use zeroes instead.

--- test/matrix_solvers.py
+++ test/matrix_solvers.py
@@ -2,45 +2,34 @@ import time
 import sys
 from numpy import *
 
-def jacobi(A, B, tol=0.005, init_val=0.0):
+def jacobi(A, B, tol=0.005, forcedIter=0):
     '''itteratively solving for matrix A with solution vector B
        tol = tolerance for dh/h
        init_val = array of initial values to use in the solver
     '''
-    dist = False
-    if(A.dist()):
-        dist = True
-    
-    print "jacobi solver - dist:", dist
-    h = zeros(shape(B), float, dist=dist)
+    print "jacobi solver - dist:", A.dist()
+    h = zeros(shape(B), float, dist=A.dist())
     dmax = 1.0
     n = 0
-
-    t1 = t = time.time()
-    while dmax > tol:
+    tmp0 = empty(shape(A), float, dist=A.dist())
+    tmp1 = empty(shape(B), float, dist=A.dist())
+    t1 = time.time()
+    while (forcedIter and forcedIter > n) or \
+          (forcedIter == 0 and dmax > tol):
         n += 1
-        #print "  Iteration = ", n
-        tmp1 = add.reduce(multiply(A,h),1)
-        tmp2 = diag(A)
-        tmp3 = subtract(B, tmp1)
-        print "TMP2: ", tmp2, " dist:",tmp2.dist()
-        print "TMP3: ", tmp3, " dist:",tmp3.dist()
-        tmp4 = divide(tmp3, tmp2)
-        hnew = h + tmp4
-##        print_arr(multiply(A,h), "multiply(A,h)")
-##        print_arr(add.reduce(multiply(A,h),1), "add.reduce(multiply(A,h))")
-##        print_arr(subtract(B, add.reduce(multiply(A,h),1)), "subtract(B, add.reduce(multiply(A,h)))")
-##        print_arr(hnew, "hnew")
-        dmax = max(abs(divide(subtract(h,hnew), h)))
-        #print "   dmax = ", dmax, ": tol = ", tol
-        #print "subtract", subtract(h,hnew)
-        if (time.time() - t) > 2.0:
-            print "Itteration = ", n, ":   dmax = ", dmax, ": tol = ", tol
-            t = time.time()
+        multiply(A,h,tmp0)
+        add.reduce(tmp0,1,out=tmp1)
+        #tmp2 = diag(A)
+        tmp2 = zeros(shape(B), float, dist=A.dist())
+        subtract(B, tmp1, tmp1)
+        divide(tmp1, tmp2, tmp1)
+        hnew = h + tmp1
+        subtract(hnew,h,tmp2)
+        divide(tmp2,h,tmp1)
+        absolute(tmp1,tmp1)
+        dmax = maximum.reduce(tmp1)
         h = hnew
-##        print n, dmax,  h
-##        print " "
-    #print_arr(h, "h")
+
     print "SOLVED at: Itteration = ", n, ":   dmax = ", dmax, ": tol = ", tol
     print "                           Time = ", time.time() - t1, "seconds"
 
@@ -55,7 +44,7 @@ def gauss_seidel(A, B, tol=0.005, init_val=0.0):
     dist = False
     if(A.dist()):
         dist = True
-    
+
     print "gauss_siedel - dist:", dist
     if init_val == 0.0:
         h = zeros(shape(B), float, dist=dist)
@@ -83,18 +72,18 @@ def gauss_seidel(A, B, tol=0.005, init_val=0.0):
             print "Itteration = ", n, ":   dmax = ", dmax, ": tol = ", tol
             t = time.time()
         h[:] = hnew[:]
-##        print n, ":", dmax,  h   
+##        print n, ":", dmax,  h
     print "SOLVED at: Itteration = ", n, ":   dmax = ", dmax, ": tol = ", tol
     print "                           Time = ", time.time() - t1, "seconds"
-    
+
     return h
 
-def sor(A, B, rf, tol=0.005, forcedIter=0):
+def sor(A, B, rf=1.3, tol=0.005, forcedIter=0):
     '''itteratively solving for matrix A with solution vector B
        rf = relaxation factor (rf > 1.0; eg rf = 1.1)
-       tol = tolerance for dh/h       
+       tol = tolerance for dh/h
     '''
-    
+
     print "successive over-relaxation (sor) - dist:", A.dist(), ""
     h = zeros(shape(B), float, dist=A.dist())
     hnew = zeros(shape(B), float, dist=A.dist())
@@ -104,12 +93,13 @@ def sor(A, B, rf, tol=0.005, forcedIter=0):
     t = time.time()
     while (forcedIter and forcedIter > n) or \
           (forcedIter == 0 and dmax > tol):
-        n += 1        
+        n += 1
+
         for i in range(len(B)):
             t1 = multiply(A[i],hnew)
             t2 = add.reduce(t1)
             hnew[i] = h[i] + (B[i] - t2) / A[i,i]
-        
+
         #over-relaxation
         dh = subtract(hnew, h)
         multiply(rf,dh,dh)
@@ -119,10 +109,10 @@ def sor(A, B, rf, tol=0.005, forcedIter=0):
         absolute(dh,dh)
         dmax = maximum.reduce(dh)
         h[:] = hnew[:]
-        
+
     print "SOLVED at: Itteration = ", n, ":   dmax = ", dmax, ": tol = ", tol
     print "                           Time = ", time.time() - t, "seconds"
-    
+
     return h
 
 
@@ -130,17 +120,15 @@ d = int(sys.argv[1])
 size = int(sys.argv[2])
 iter = int(sys.argv[3])
 
-A = array([[4, -1, -1, 0], [-1, 4, 0, -1], [-1, 0, 4, -1], [0, -1, -1, 4]], float, dist=d)
-B = array([1,2,0,1], float, dist=d)
+#A = array([[4, -1, -1, 0], [-1, 4, 0, -1], [-1, 0, 4, -1], [0, -1, -1, 4]], float, dist=d)
+#B = array([1,2,0,1], float, dist=d)
 
 
-#A = array(random_list([size,size]), float, dist=d)
-#A = zeros([size,size], dtype=float, dist=d)
-#add(A,42,A)
+A = zeros([size,size], dtype=float, dist=d)
+ufunc_random(A,A)
 
-# = array(random_list([size]), float, dist=d)
-#B = zeros([size], dtype=float, dist=d)
-#add(B,42,B)
+B = zeros([size], dtype=float, dist=d)
+ufunc_random(B,B)
 
-C = sor(A, B, rf=1.3)
-print C
+C = jacobi(A, B, forcedIter=iter)
+
