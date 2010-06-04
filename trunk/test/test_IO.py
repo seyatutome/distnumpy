2bc3911b8b35299b8155b2f8965b1b51028ca428
madsbk <madsbk@797bad1c-92e7-11de-9a30-078c9dd406eb>
2010-06-04 11:12:37 +0000

    The file used in test_IO.py was saved in /tmp, which is not shared on NFS. Fixed

--- trunk/test/test_IO.py
+++ trunk/test/test_IO.py
@@ -1,13 +1,14 @@
 import numpy as np
 import dnumpytest
 import random
+import subprocess
 
 def run():
     max_ndim = 6
     for i in xrange(1,max_ndim+1):
         src = dnumpytest.random_list(random.sample(xrange(1, 10),i))
         A = np.array(src, dtype=float, dist=True)
-        fname = "%sdistnumpt_test_matrix.npy"%dnumpytest.TmpSetDir
+        fname = "%sdistnumpt_test_matrix.npy"
         np.save(fname,A)
         Bf = np.load(fname, dist=False)
         Bd = np.load(fname, dist=True)
@@ -16,7 +17,9 @@ def run():
             err = "Input array:\n %s\n"%str(A)
             err += "The loaded array from DistNumPy:\n%s\n"%str(Bd)
             err += "The loaded array from NumPy:\n%s\n"%str(Bf)
+            subprocess.check_call(('rm %s'%fname), shell=True)
             return (True, err)
+        subprocess.check_call(('rm %s'%fname), shell=True)
     return (False, "")
 
 if __name__ == "__main__":
