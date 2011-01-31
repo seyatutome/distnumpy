24bda585d0c6f1b45f40f99a0b1248e34aedd1e6
madsbk <madsbk@797bad1c-92e7-11de-9a30-078c9dd406eb>
2011-01-31 15:43:09 +0000

    Create branch no_latency_hiding

---
+++ branches/no_latency_hiding/benchmarks/jacobi_stencil.py
@@ -0,0 +1,54 @@
+import numpy as np
+import sys
+import time
+
+DIST=int(sys.argv[1])
+
+W = int(sys.argv[2])
+H = int(sys.argv[2])
+
+forcedIter = int(sys.argv[3])
+
+full = np.zeros((W+2,H+2), dtype=np.double, dist=DIST)
+work = np.zeros((W,H), dtype=np.double, dist=DIST)
+diff = np.zeros((W,H), dtype=np.double, dist=DIST)
+tmpdelta = np.zeros((W), dtype=np.double, dist=DIST)
+
+cells = full[1:-1, 1:-1]
+up    = full[1:-1, 0:-2]
+left  = full[0:-2, 1:-1]
+right = full[2:  , 1:-1]
+down  = full[1:-1, 2:  ]
+
+full[:,0]  += -273.15
+full[:,-1] += -273.15
+full[0,:]  +=  40.0
+full[-1,:] += -273.13
+
+t1 = time.time()
+
+epsilon=W*H*0.010
+delta=epsilon+1
+i=0
+while (forcedIter and forcedIter > i) or \
+      (forcedIter == 0 and epsilon<delta):
+  i+=1
+  work[:] = cells
+  work += up
+  work += left
+  work += right
+  work += down
+  work *= 0.2
+  np.subtract(cells,work,diff)
+  np.absolute(diff, diff)
+  np.add.reduce(diff,out=tmpdelta)
+  delta = np.add.reduce(tmpdelta)
+  cells[:] = work
+
+t2 = time.time()
+print 'Iter: ', i, ' size: ', H,' time: ', t2-t1,
+if DIST:
+    print "(Dist) notes: %s"%sys.argv[4]
+else:
+    print "(Non-Dist) notes: %s"%sys.argv[4]
+
---
+++ branches/no_latency_hiding/test/test_jacobi.py
@@ -0,0 +1,40 @@
+from numpy import *
+import dnumpytest
+
+def jacobi(A, B, tol=0.005):
+    '''itteratively solving for matrix A with solution vector B
+       tol = tolerance for dh/h
+       init_val = array of initial values to use in the solver
+    '''
+    h = zeros(shape(B), float, dist=A.dist())
+    dmax = 1.0
+    n = 0
+    tmp0 = empty(shape(A), float, dist=A.dist())
+    tmp1 = empty(shape(B), float, dist=A.dist())
+    AD = diagonal(A)
+    while dmax > tol:
+        n += 1
+        multiply(A,h,tmp0)
+        add.reduce(tmp0,1,out=tmp1)
+        tmp2 = AD
+        subtract(B, tmp1, tmp1)
+        divide(tmp1, tmp2, tmp1)
+        hnew = h + tmp1
+        subtract(hnew,h,tmp2)
+        divide(tmp2,h,tmp1)
+        absolute(tmp1,tmp1)
+        dmax = maximum.reduce(tmp1)
+        h = hnew
+    return h
+
+def run():
+    A = load("%sJacobi_Amatrix.npy"%dnumpytest.DataSetDir, dist=True)
+    B = load("%sJacobi_Bvector.npy"%dnumpytest.DataSetDir, dist=True)
+    C = load("%sJacobi_Cvector.npy"%dnumpytest.DataSetDir, dist=True)
+    result = jacobi(A,B)
+
+    if not dnumpytest.array_equal(C,result):
+        raise Exception("Uncorrect result vector\n")
+
+if __name__ == "__main__":
+    run()
