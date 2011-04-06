a6fb80f4a2dbadfb3ef8a3e99fc307b0ebb8c08c
madsbk <madsbk@797bad1c-92e7-11de-9a30-078c9dd406eb>
2011-04-06 08:59:34 +0000

    added a lazyiness value, which most be greater than DNPY_MAX_VB_IN_SVB_DAG or 1. This makes the no-latency branch obsolete.

--- dnumpy/numpy/core/include/numpy/distnumpy.h
+++ dnumpy/numpy/core/include/numpy/distnumpy.h
@@ -45,6 +45,10 @@ typedef struct {
 //Maximum number of view block operations in the sub-view-block DAG.
 #define DNPY_MAX_VB_IN_SVB_DAG 1000
 
+//Maximum number of nodes in the dependency system.
+//NB: must be greater than DNPY_MAX_VB_IN_SVB_DAG or 1.
+#define DNPY_MAX_LAZYINESS 10000
+
 //Maximum number of allocated arrays
 #define DNPY_MAX_NARRAYS 1024
 
