9854dd9b867049a0e110dc933d25423fd81f1e66
madsbk <madsbk@797bad1c-92e7-11de-9a30-078c9dd406eb>
2011-04-14 10:50:00 +0000

    rewritten kNN.py and updated the parsing.

--- benchmarks/kNN.py
+++ benchmarks/kNN.py
@@ -2,39 +2,47 @@ import numpy as np
 import time
 import sys
 
-def compute_targets_pyloop(base, targets):
-    dist = (base[0]-targets[0])**2
-    for i in range(1,len(base)):
-        dist += (base[i]-targets[i])**2
+def compute_targets_pyloop(base, target):
+    tmp = base-target[:,np.newaxis]
+    dist= np.add.reduce(tmp)**2
     dist = np.sqrt(dist)
     r  = np.max(dist, axis=0)
     return r
 
-def compute_targets(base, target):
-    dist = (base-target[:,np.newaxis])**2
-    dist = np.add.reduce(dist)
-    dist = np.sqrt(dist)
-    r  = np.max(dist, axis=0)
+def compute_targets(base, target, tmp1, tmp2):
+    base = base[:,np.newaxis]
+    target = target[:,:,np.newaxis]
+    t = base - target
+    #print t.shape, tmp1.shape
+    np.subtract(base, target, tmp1)
+    tmp1 **= 2
+    np.add.reduce(tmp1, out=tmp2)
+    np.sqrt(tmp2, tmp2)
+    r  = np.max(tmp2, axis=0)
     return r
 
 DIST = int(sys.argv[1])
 ndims = int(sys.argv[2])
 db_length = int(sys.argv[3])
-niters = int(sys.argv[4])
+step = int(sys.argv[4])
 
-targets = np.ufunc_random(np.empty((ndims, db_length), dtype=float, dist=DIST))
-base = np.ufunc_random(np.empty((ndims,db_length), dtype=float, dist=DIST))
+targets = np.ufunc_random(np.empty((ndims,db_length), dtype=float, dist=DIST))
+base = targets #np.ufunc_random(np.empty((ndims,db_length), dtype=float, dist=DIST))
+
+tmp1 = np.empty((ndims,step,step), dtype=float, dist=DIST)
+tmp2 = np.empty((step,step), dtype=float, dist=DIST)
 
 np.core.multiarray.timer_reset()
 np.core.multiarray.evalflush()
 t1 = time.time()
-for i in xrange(niters):
-    compute_targets_pyloop(base, targets[:,i])
+for i in xrange(0, db_length, step):
+    for j in xrange(0, db_length, step):
+        compute_targets(base[:,i:i+step], targets[:,j:j+step], tmp1, tmp2)
 np.core.multiarray.evalflush()
 t2 = time.time()
 
-print "ndims: %d, dbsize: %d, niters: %d - time: %f sec."\
-      %(ndims, db_length, niters, t2-t1),
+print "ndims: %d, dbsize: %d, step: %d - time: %f sec."\
+      %(ndims, db_length, step, t2-t1),
 if DIST:
     print "(Dist) notes: %s"%sys.argv[5]
 else:
