d6d9844afd6ad99442194c010bcc06e8a7eef3df
madsbk <madsbk@797bad1c-92e7-11de-9a30-078c9dd406eb>
2011-06-15 21:17:51 +0000

    Fixed a bug in SUMMA and added views to the SUMMA test. At the moment SUMMA does not support views as output

--- pyHPC/summa.py
+++ pyHPC/summa.py
@@ -6,28 +6,33 @@ def summa(a,b,out=None):
     Matrix multiplication using the SUMMA algorithm.
 
     """
-    row   = a.shape[0] #Number of rows in output matrix
-    col   = b.shape[1] #Number of columns in output matrix
     ksize = a.shape[1] #Number of rows and columns in the match dimension
 
     if out is None:
-        c = np.zeros((row,col), dtype=a.dtype, dist=True)
+        c = np.zeros((a.shape[0],b.shape[1]), dtype=a.dtype, dist=True)
     else:
         c = out
 
-    if a.shape[1] != b.shape[0]:
-        raise Exception("Shape of a and b does not match")
+    if a.ndim != 2 or b.ndim != 2 or c.ndim != 2:
+        raise Exception("All arrays must have two dimensions")
 
-    if c.shape[0] != row or c.shape[1] != col:
-        raise Exception("Shape of output do not match input")
+    if a.shape[1] != b.shape[0]:
+        raise Exception("Shape of a(%d,%d) and b(%d,%d) does not match"\
+                         %(a.shape[0],a.shape[1],b.shape[0],b.shape[1]))
+
+    if c.shape[0] != a.shape[0] or c.shape[1] != b.shape[1]:
+        raise Exception("Shape of output (%d,%d) do not match input "\
+                        "a(%d,%d) and b(%d,%d)"\
+                        %(c.shape[0],c.shape[1],a.shape[0],\
+                        a.shape[1],b.shape[0],b.shape[1]))
 
     if not (a.dist() and b.dist() and c.dist()):
         raise Exception("All arrays must be distributed")
 
     (prow,pcol) = a.pgrid()
     BS = np.BLOCKSIZE
-    a_work = np.zeros((col,BS*pcol), dtype=a.dtype, dist=True)
-    b_work = np.zeros((BS*prow,row), dtype=a.dtype, dist=True)
+    a_work = np.zeros((a.shape[0],BS*pcol), dtype=a.dtype, dist=True)
+    b_work = np.zeros((BS*prow,b.shape[1]), dtype=a.dtype, dist=True)
 
     for k in xrange(0,ksize,BS):
         bs = min(BS, ksize - k)#Current block size
--- test/test_summa.py
+++ test/test_summa.py
@@ -10,12 +10,13 @@ def run():
         print "[rank %d] Warning - ignored in non-SPMD mode\n"%(np.RANK),
         return
 
+    #Non-view test - identical to the one in test_dot.py
     niter = 6
     for m in range(2,niter+2):
         for n in range(2,niter+2):
             for k in range(2,niter+2):
                 Asrc = dnumpytest.random_list([k,m])
-                Bsrc = dnumpytest.random_list([m,k])
+                Bsrc = dnumpytest.random_list([n,k])
                 Ad = np.array(Asrc, dtype=float, dist=True)
                 Af = np.array(Asrc, dtype=float, dist=False)
                 Bd = np.array(Bsrc, dtype=float, dist=True)
@@ -23,11 +24,27 @@ def run():
                 Cd = summa.summa(Ad,Bd)
                 Cf = np.dot(Af,Bf)
                 if not dnumpytest.array_equal(Cd,Cf):
-                    print Af
-                    print Bf
-                    print Cf
-                    print Cd
                     raise Exception("Uncorrect result matrix\n")
-
+    niter *= 2
+    Asrc = dnumpytest.random_list([niter,niter])
+    Bsrc = dnumpytest.random_list([niter,niter])
+    Ad = np.array(Asrc, dtype=float, dist=True)
+    Af = np.array(Asrc, dtype=float, dist=False)
+    Bd = np.array(Bsrc, dtype=float, dist=True)
+    Bf = np.array(Bsrc, dtype=float, dist=False)
+    Cd = np.zeros((niter,niter),dtype=float, dist=True)
+    BS = np.BLOCKSIZE
+    for m in xrange(0,niter-BS, BS):
+        for n in xrange(0,niter-BS,BS):
+            for k in xrange(0,niter-BS,BS):
+                tAd = Ad[m:,k:]
+                tAf = Af[m:,k:]
+                tBd = Bd[k:,n:]
+                tBf = Bf[k:,n:]
+                tCd = Cd[m:,n:]
+                tCd = summa.summa(tAd,tBd)
+                tCf = np.dot(tAf,tBf)
+                if not dnumpytest.array_equal(tCd,tCf):
+                    raise Exception("Uncorrect result matrix\n")
 if __name__ == "__main__":
     run()
