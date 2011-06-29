02b9e1a4a0cc30dc421cf64d801e98a851f079d6
madsbk <madsbk@797bad1c-92e7-11de-9a30-078c9dd406eb>
2011-06-29 00:57:22 +0000

    Added speedup support

--- benchmarks/parsing/dndoutput.py
+++ benchmarks/parsing/dndoutput.py
@@ -6,14 +6,17 @@ import pickle
 
 #########Processing Script#############
 if __name__ == "__main__":
-    FIND_SPEEDUP = 0
+    #-1: no speedup, 0: speedup (NumPy), 1+: speedup (DistNumPy)
+    FIND_SPEEDUP = -1
     MAX_NP = 16 #-1 #Unlimited number of processors
 
-    options, remainders = getopt.gnu_getopt(sys.argv[1:], '', ['max_np='])
+    options, remainders = getopt.gnu_getopt(sys.argv[1:], '', ['max_np=', 'speedup='])
 
     for opt, arg in options:
         if opt in ('--max_np'):
             MAX_NP = int(arg)
+        if opt in ('--speedup'):
+            FIND_SPEEDUP = int(arg)
 
     #Read input files
     inputfiles = []
@@ -27,7 +30,10 @@ if __name__ == "__main__":
     #are under the same key.
     inputs = {}
     for i in inputfiles:
-        ws = i['WORLDSIZE']
+        if i['dist']:
+            ws = i['WORLDSIZE']
+        else:
+            ws = 0 #Zero means sequential execution.
         if ws in inputs:
             inputs[ws].append(i)
         else:
@@ -61,15 +67,34 @@ if __name__ == "__main__":
                              i['reduce_nd'] - i['reduce_nd_apply'] + \
                              i['msg2slaves']
 
+    #Compute average values.
+    result = {}
+    for (k,v) in inputs.items():
+        run  = numpy.array([i['total'] for i in v], dtype=float)
+        wait = numpy.array([i['wait_time'] for i in v], dtype=float)
+        result[k] = (numpy.average(run),numpy.average(wait))
+
     #Print CSV file
-    print 'CPUs;Runtime(node);Commtime(node)'
-
-    keys=inputs.keys()
+    keys=result.keys()
     keys.sort()
+    if 0 in result:#Sequential runtime
+        SEQ = "#  Seq;%8d;\n"%(result[0][0]/1000)
+        keys.pop(0)#Remote the zero key.
+    else:
+        SEQ = ""
+    if FIND_SPEEDUP > -1: #Show speedup instead of runtime.
+        speedup = result[FIND_SPEEDUP][0]#The speedup baseline
+        print 'CPUs;Speedup;Commtime'
+        print SEQ,
+        for k in keys:
+            runtime  = speedup / result[k][0]
+            waittime = int(result[k][1] / 1000)# convert to in ms
+            print "%6d;%8.3f;%8d"%(k, runtime, waittime)
+    else:
+        print 'CPUs;Runtime;Commtime'
+        print SEQ,
+        for k in keys:
+            runtime  = int(result[k][0] / 1000)# convert to in ms
+            waittime = int(result[k][1] / 1000)# convert to in ms
+            print "%6d;%8d;%8d"%(k, runtime, waittime)
 
-    for k in keys:
-        runtime = numpy.array([v['total'] for v in inputs[k]], dtype=float)
-        runtime = int(numpy.average(runtime) / 1000)# in ms
-        waittime = numpy.array([v['wait_time'] for v in inputs[k]], dtype=float)
-        waittime = int(numpy.average(waittime) / 1000) # in ms
-        print "%6d;%8d;%8d"%(k, runtime, waittime)
