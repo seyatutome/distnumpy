72e09d520fdf2b562e1ac0ee34afdcac1504a3db
madsbk <madsbk@797bad1c-92e7-11de-9a30-078c9dd406eb>
2011-08-05 23:52:13 +0000

    The LU test will now only use matrixes with dimensions that are divisible with np.BLOCKSIZE.

--- trunk/test/test_lu.py
+++ trunk/test/test_lu.py
@@ -2,6 +2,29 @@
 import numpy as np
 import dnumpytest
 
+def gen_matrix(SIZE,dist):
+    BS = np.BLOCKSIZE
+    Nblock = SIZE / BS
+    Nlocal = BS
+
+    # generate matrix to factorize
+    A = np.zeros((SIZE,SIZE), dtype=float, dist=dist);
+    for row in xrange(0,SIZE,BS):
+        rbs = min(BS,SIZE - row)
+        for col in xrange(0,SIZE,BS):
+            cbs = min(BS,SIZE - col)
+            for n in xrange(rbs):
+                for m in xrange(cbs):
+                    r = row + n
+                    c = col + m
+                    t1 = float(SIZE) / BS
+                    A[row+n,col+m] = r*c / (t1*BS*BS*t1)
+            if row == col:
+                for n in xrange(rbs):
+                    A[row+n,col+n] = A[row+n,col+n] + 10
+    return A
+
+
 def run():
     if not np.SPMD_MODE:
         print "[rank %d] Warning - ignored in non-SPMD mode\n"%(np.RANK),
@@ -17,28 +40,10 @@ def run():
         print "[rank %d] Warning - ignored pyHPC not found\n"%(np.RANK),
         return
 
-    for SIZE in xrange(1,80):
-        BS = np.BLOCKSIZE
-        Nblock = SIZE / BS
-        Nlocal = BS
-        # generate matrix to factorize
-        A = np.zeros((SIZE,SIZE), dtype=float);
-        for row in xrange(0,SIZE,BS):
-            rbs = min(BS,SIZE - row)
-            for col in xrange(0,SIZE,BS):
-                cbs = min(BS,SIZE - col)
-                for n in xrange(rbs):
-                    for m in xrange(cbs):
-                        r = row + n
-                        c = col + m
-                        t1 = float(SIZE) / BS
-                        A[row+n,col+m] = r*c / (t1*BS*BS*t1)
-                if row == col:
-                    for n in xrange(rbs):
-                        A[row+n,col+n] = A[row+n,col+n] + 10
+    for SIZE in xrange(np.BLOCKSIZE,100,np.BLOCKSIZE):
+        (Ld, Ud) = pyHPC.lu(gen_matrix(SIZE,True))
+        (P, Lf, Uf) = linalg.lu(gen_matrix(SIZE,False))
 
-        (Ld, Ud) = pyHPC.lu(A.copy())
-        (P, Lf, Uf) = linalg.lu(A)
         #There seems to be a transpose bug in SciPy's LU
         Lf = Lf.T
         Uf = Uf.T
