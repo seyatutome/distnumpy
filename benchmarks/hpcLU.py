70bd2e4b4a2d845be51055b0fc5223a6ea79b9d2
madsbk <madsbk@797bad1c-92e7-11de-9a30-078c9dd406eb>
2011-08-09 21:01:34 +0000

    Added a pyHPC.lu benchmark

---
+++ benchmarks/hpcLU.py
@@ -0,0 +1,33 @@
+import time
+import numpy as np
+import pyHPC
+import util
+from scipy import linalg
+
+def gen_matrix(SIZE,dist):
+    A = np.zeros((SIZE,SIZE), dtype=float, dist=dist);
+    np.ufunc_random(A,A)
+    for i in xrange(SIZE):
+        A[i,i] *= 1000000.0
+    return A
+
+parser = util.Parsing()
+DIST = parser.dist
+SIZE = int(parser.argv[0])
+A = gen_matrix(SIZE, DIST)
+
+if DIST:
+    f = pyHPC.lu
+else:
+    f = linalg.lu
+
+np.timer_reset()
+f(A)
+timing = np.timer_getdict()
+
+if np.RANK == 0:
+    print 'hpcLU - size:,',np.shape(A)
+
+parser.pprint(timing)
+parser.write_dict(timing)
+
