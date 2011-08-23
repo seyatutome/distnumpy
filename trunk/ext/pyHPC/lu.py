491a47320c44ef9b00a5c213193c6a73b95fd445
madsbk <madsbk@797bad1c-92e7-11de-9a30-078c9dd406eb>
2011-08-23 22:50:38 +0000

    SUMMA now supports offsets into the input arrays

--- trunk/ext/pyHPC/lu.py
+++ trunk/ext/pyHPC/lu.py
@@ -3,60 +3,6 @@ from scipy import linalg
 import pyHPC
 from itertools import izip as zip
 
-def luSEQ(A):
-    """
-    Compute LU decompostion of a matrix.
-
-    Parameters
-    ----------
-    a : array, shape (M, M)
-        Array to decompose
-
-    Returns
-    -------
-    p : array, shape (M, M)
-        Permutation matrix
-    l : array, shape (M, M)
-        Lower triangular or trapezoidal matrix with unit diagonal.
-    u : array, shape (M, M)
-        Upper triangular or trapezoidal matrix
-    """
-
-    if A.shape[0] != A.shape[0]:
-        raise Exception("LU only supports squared matricis")
-    SIZE = A.shape[0]
-    BS = np.BLOCKSIZE
-    L = np.zeros((SIZE,SIZE), dtype=float)
-    U = np.zeros((SIZE,SIZE), dtype=float)
-
-    for k in xrange(0,SIZE,BS):
-        bs = min(BS,SIZE - k) #Current block size
-
-        diagA = A[k:k+bs,k:k+bs]
-        (p,diagL,diagU) = linalg.lu(diagA)
-        if not (np.diag(p) == 1).all():#We do not support pivoting
-            raise Exception("Pivoting was needed!")
-
-        #There seems to be a transpose bug in SciPy's LU
-        diagL = diagL.T
-        diagU = diagU.T
-
-        L[k:k+bs,k:k+bs] = diagL
-        U[k:k+bs,k:k+bs] = diagU
-
-        if k+bs < SIZE:
-            #Compute multipliers
-            for i in xrange(k+bs,SIZE,BS):
-                tbs = min(BS,SIZE - i) #Block size of the i'th block
-                L[i:i+tbs,k:k+bs] = np.linalg.solve(diagU.T, A[i:i+tbs,k:k+bs].T).T
-                U[k:k+bs,i:i+tbs] = np.linalg.solve(diagL , A[k:k+bs,i:i+tbs])
-            #Apply to remaining submatrix
-            A[k+bs:, k+bs:] -= np.dot(L[k+bs:,k:k+bs], U[k:k+bs,k+bs:])
-
-    A = L + U - np.identity(SIZE) #Merge L and U into A.
-
-    return (L, U)
-
 def lu(matrix):
     """
     Compute LU decompostion of a matrix.
@@ -125,6 +71,8 @@ def lu(matrix):
                 l[:] = np.linalg.solve(l,a)
 
             #Apply to remaining submatrix
-            A[k+bs:,k+bs:] -= pyHPC.summa(L[k+bs:,k:k+bs],U[k:k+bs,k+bs:])
+            tmp = pyHPC.summa(L[:,:k+bs],U[:k+bs,:], ao=(k+bs,k),
+                              bo=(k,k+bs), co=(k+bs,k+bs))
+            A += tmp
 
     return (L, U)
--- trunk/ext/pyHPC/summa.py
+++ trunk/ext/pyHPC/summa.py
@@ -1,24 +1,24 @@
 #SUMMA: matrix multiplication algorithm
 import numpy as np
 
-def summa(a,b):
+def summa(a,b,c=None, ao=(0,0),bo=(0,0),co=(0,0)):
     """
     Matrix multiplication using the SUMMA algorithm.
     Views of arrays is allowed if there are aligned to the global
     blocksize
     """
-    ksize = a.shape[1] #Number of rows and columns in the match dimension
 
-    c = np.zeros((a.shape[0],b.shape[1]), dtype=a.dtype, dist=True)
+    if c is None:
+        c = np.zeros((a.shape[0],b.shape[1]), dtype=a.dtype, dist=True)
 
     if a.ndim != 2 or b.ndim != 2 or c.ndim != 2:
         raise Exception("All arrays must have two dimensions")
 
-    if a.shape[1] != b.shape[0]:
+    if a.shape[1]-ao[1] != b.shape[0]-bo[0]:
         raise Exception("Shape of a(%d,%d) and b(%d,%d) does not match"\
                          %(a.shape[0],a.shape[1],b.shape[0],b.shape[1]))
 
-    if c.shape[0] != a.shape[0] or c.shape[1] != b.shape[1]:
+    if c.shape[0]-co[0] != a.shape[0]-ao[0] or c.shape[1]-co[1] != b.shape[1]-bo[1]:
         raise Exception("Shape of output (%d,%d) do not match input "\
                         "a(%d,%d) and b(%d,%d)"\
                         %(c.shape[0],c.shape[1],a.shape[0],\
@@ -27,11 +27,21 @@ def summa(a,b):
     if not (a.dist() and b.dist() and c.dist()):
         raise Exception("All arrays must be distributed")
 
+    #Allocate work arrays
     (prow,pcol) = a.pgrid()
     BS = np.BLOCKSIZE
     a_work = np.zeros((a.shape[0],BS*pcol), dtype=a.dtype, dist=True)
     b_work = np.zeros((BS*prow,b.shape[1]), dtype=a.dtype, dist=True)
 
+    #Apply offset
+    a_work = a_work[ao[0]:,:]
+    b_work = b_work[:,bo[1]:]
+    a = a[ao[0]:,ao[1]:]
+    b = b[bo[0]:,bo[1]:]
+    c_new = c[co[0]:,co[1]:]
+
+    ksize = a.shape[1]#Number of rows and columns in the match dimension
+    #SUMMA
     for k in xrange(0,ksize,BS):
         bs = min(BS, ksize - k)#Current block size
 
@@ -45,8 +55,7 @@ def summa(a,b):
         #Apply local outer dot product
         l_a_work = a_work.local()[:,:bs]
         l_b_work = b_work.local()[:bs,:]
-        l_c = c.local()
+        l_c = c_new.local()
         if l_c.size > 0:
             l_c += np.dot(l_a_work, l_b_work)
-
     return c
