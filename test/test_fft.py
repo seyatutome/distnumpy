ad011c4cb35f5d33b882beda6c2277a3b6358ee9
madsbk <madsbk@797bad1c-92e7-11de-9a30-078c9dd406eb>
2011-08-31 19:27:47 +0000

    Added a 3D FFT to pyHPC

--- test/test_fft.py
+++ test/test_fft.py
@@ -5,7 +5,7 @@ import dnumpytest
 
 def run():
     #Make sure we have one non-distributed dimension.
-    np.datalayout([(2,1,1)])
+    np.datalayout([(2,1,1),(3,1,1)])
     if not np.SPMD_MODE:
         print "[rank %d] Warning - ignored in non-SPMD mode\n"%(np.RANK),
         return
@@ -23,6 +23,7 @@ def run():
         print "[rank %d] Warning - ignored np.BLOCKSIZE too high\n"%(np.RANK),
         return
 
+    #2D FFT
     for SIZE in xrange(np.BLOCKSIZE,np.BLOCKSIZE*10,np.BLOCKSIZE):
         src = dnumpytest.random_list([SIZE,SIZE])
         Ad = np.array(src, dtype=np.complex, dist=True)
@@ -31,6 +32,17 @@ def run():
         Bf = np.fft.fft2(Af)
 
         if not dnumpytest.array_equal(Bf,Bd,maxerror=1e-6):
+            raise Exception("Uncorrect result array\n")
+
+    #3D FFT
+    for SIZE in xrange(np.BLOCKSIZE,np.BLOCKSIZE*5,np.BLOCKSIZE):
+        src = dnumpytest.random_list([SIZE,SIZE,SIZE])
+        Ad = np.array(src, dtype=np.complex, dist=True)
+        Af = np.array(src, dtype=np.complex, dist=False)
+        Bd = pyHPC.fft3d(Ad)
+        Bf = np.fft.fftn(Af)
+
+        if not dnumpytest.array_equal(Bd,Bf,maxerror=1e-6):
             raise Exception("Uncorrect result array\n")
 
 if __name__ == "__main__":
