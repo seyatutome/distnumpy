d080e0f0fb08b275455427cfcb3b93b0fdabca48
madsbk <madsbk@797bad1c-92e7-11de-9a30-078c9dd406eb>
2011-09-02 21:22:57 +0000

    Added a FFT 2D benchmark

---
+++ trunk/benchmarks/fft2d.py
@@ -0,0 +1,25 @@
+import time
+import numpy as np
+import pyHPC
+import util
+
+parser = util.Parsing()
+DIST = parser.dist
+SIZE = int(parser.argv[0])
+A = np.empty((SIZE,SIZE), dtype=np.complex, dist=DIST)
+
+if DIST:
+    f = pyHPC.fft2d
+else:
+    f = np.fft.fft2d
+
+np.timer_reset()
+f(A)
+timing = np.timer_getdict()
+
+if np.RANK == 0:
+    print 'fft2d - size:,',np.shape(A)
+
+parser.pprint(timing)
+parser.write_dict(timing)
+
